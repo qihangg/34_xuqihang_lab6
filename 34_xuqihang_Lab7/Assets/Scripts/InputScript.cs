401fd7f27e06f2a8dd0d2366d0264edfd831daa3
qihangg <122329456+qihangg@users.noreply.github.com>
2023-01-19 18:03:21 +0800

    lab6 by qihang

---
+++ 34_xuqihang_Lab7/Assets/Scripts/InputScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputScript : MonoBehaviour
+{
+    public Toggle USD;
+    public Toggle JPY;
+
+    public Text value;
+
+    public string input;
+    public double InputValue;
+  
+    public void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (USD.isOn)
+        {
+            JPY.isOn = false;
+        }
+        else if (JPY.isOn)
+        {
+            USD.isOn = false;
+        }
+        
+       
+        
+    }
+
+    public void ReadStringInput(string s)
+    {
+        input = s;
+
+        double.TryParse(input, out InputValue);
+    }
+
+    public void convert_button()
+    {
+        if (USD.isOn)
+        {
+            InputValue *= 0.74f;
+            value.text = "" + InputValue;
+        }
+        if (JPY.isOn)
+        {
+            InputValue *= 97.14f;
+            value.text = "" + InputValue;
+        }
+
+
+       
+    }
+
+    public void clear_button(InputField s)
+    {
+        s.text = "" + 0;
+        value.text = "" + s;
+    }
+}
